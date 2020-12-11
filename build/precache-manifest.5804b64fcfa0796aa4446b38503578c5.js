845db661cf15992ab2338d042c4bbe3cba96cada
WallysLS <wallys31.souza@gmail.com>
2020-12-11 19:49:40 -0300

    Build

---
+++ build/precache-manifest.5804b64fcfa0796aa4446b38503578c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cfc93fc727e17891f9abc7a0933e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb3f6d2215299d1af24f",
+    "url": "/static/css/main.9d97ae6d.chunk.css"
+  },
+  {
+    "revision": "921f1de19c64cd69c46b",
+    "url": "/static/js/2.675ddd42.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.675ddd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3f6d2215299d1af24f",
+    "url": "/static/js/main.f4fff0f4.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
