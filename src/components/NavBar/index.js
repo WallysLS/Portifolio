480aaea1b60ab2bae7205c2090ab0c33617f6144
WallysLS <wallys31.souza@gmail.com>
2020-12-11 19:56:56 -0300

    Build

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+  const [ setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -17,9 +17,7 @@ function Navbar() {
     }
   };
 
-  useEffect(() => {
-    showButton();
-  }, []);
+  
 
   window.addEventListener('resize', showButton);
 
